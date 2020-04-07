ebcd1a94dc6241b56fc08f6046628356372b6065
Shea Murphy <Shea.Murphy@nology.io>
2020-04-07 20:07:45 +0100

    Not sure where this has gone

--- crash-course/main.js
+++ crash-course/main.js
@@ -11,7 +11,7 @@ let camera = new THREE.PerspectiveCamera(
 camera.position.z = 5;
 
 let renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setClearColor('#e5e5e5');
+renderer.setClearColor('#F0F8FF');
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
@@ -26,34 +26,82 @@ window.addEventListener('resize', () => {
 // SHAPES
 
 // SPHERE
-// let geometry = new THREE.SphereGeometry(1, 100, 100);
+let geometry = new THREE.SphereGeometry(1, 100, 100);
 
 // Box
-let geometry = new THREE.BoxGeometry(1, 1);
+// let geometry = new THREE.BoxGeometry(1, 1, 0.5);
 let material = new THREE.MeshLambertMaterial({ color: 0xFFCC00 });
-let mesh = new THREE.Mesh(geometry, material);
+let mesh = new THREE.Mesh(geometry, material)
+mesh.position.y = -2;
 let meshPostionX = 0;
 
+let blockGeo = new THREE.BoxGeometry(1000, 0, 0);
+let blockMaterial = new THREE.MeshLambertMaterial({ color: 0xefefee });
+let blockMesh = new THREE.Mesh(blockGeo, blockMaterial);
+blockMesh.position.y = -2;
+blockMesh.position.z = 2;
+
+
+// rabbitMesh.position.z = 2;
+
 scene.add(mesh);
+scene.add(blockMesh);
 
 let light = new THREE.PointLight(0xFFFFFF, 1, 500);
-light.position.set(10, 0, 25);
-scene.add(light);
+let lightPostions = [-100, 0, 50];
+
+const setUpLight = () => {
+    light.position.set(...lightPostions);
+    scene.add(light);
+}
+
+const moveLight = () => {
+    if (lightPostions[0] < 0) {
+        lightPostions[0] += 1;
+        lightPostions[1] += 1;
+    } else if (lightPostions[0] < 100) {
+        lightPostions[0] += 1;
+        lightPostions[1] -= 1;
+    }
+}
 
 // Recursive function redraws every time screen is refreshed (around 60 frames per second)
 const render = () => {
     requestAnimationFrame(render);
     mesh.position.x = meshPostionX;
+    moveLight();
+    setUpLight();
     renderer.render(scene, camera);
 }
 
 render();
 
-const moveShape = (direction) => {
+// const moveShape = (direction) => {
+//     direction === 'left' ? meshPostionX -= 0.1 : meshPostionX += 0.1;
+// }
 
-    direction === 'left' ? meshPostionX -= 1 : meshPostionX += 1;
+// window.addEventListener('keydown', (e) => {
+//     e.keyCode === 37 ? moveShape('left') : e.keyCode === 39 ? moveShape('right') : null;
+// })
+
+let currentPos = 30;
+
+const moveRabbit = (direction) => {
+    direction === 'left' ? currentPos -= 10 : currentPos += 10;
+    rabbit.style.left = `${currentPos}px`
 }
 
-window.addEventListener('keyup', (e) => {
-    e.keyCode === 37 ? moveShape('left') : e.keyCode === 39 ? moveShape('right') : null;
+window.addEventListener('keydown', (e) => {
+    e.keyCode === 37 ? moveRabbit('left') : e.keyCode === 39 ? moveRabbit('right') : null;
 })
+
+// GSAP (2nd CDN) Timeline for stringing animations
+
+// this.tl = new TimelineMax().delay(.3);
+// this.tl.to(mesh.scale, 1, { x: 2, ease: Expo.easeOut })
+// this.tl.to(mesh.scale, 1, { y: 2, ease: Expo.easeOut }, "-=1")
+// this.tl.to(mesh.scale, 1, { z: 2, ease: Expo.easeOut }, "-=1")
+// this.tl.to(mesh.rotation, 3, { y: 4, ease: Expo.easeOut })
+const rabbit = document.getElementById('rabbit');
+rabbit.style.left = '30px';
+setTimeout(() => rabbit.classList.remove('rabbitLoad'), 6000)
