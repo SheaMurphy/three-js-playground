b82a4e5a58ea51132be53d2ffc252f0a3804f09f
Shea Murphy <Shea.Murphy@nology.io>
2020-04-12 16:06:15 +0100

    Got skeleton of castle

--- groups/main.js
+++ groups/main.js
@@ -18,11 +18,12 @@ const init = () => {
         1,
         1000
     );
-    camera.position.z = 100;
+    camera.position.z = 150;
+    camera.position.y = 125;
 
     // CONTROLS
     controls = new THREE.TrackballControls(camera, renderer.domElement);
-    controls.addEventListener('change', render);
+    controls.addEventListener('change', render)
 
     // SCENE
     scene = new THREE.Scene();
@@ -56,30 +57,6 @@ const handleResize = () => {
     controls.handleResize();
 }
 
-const buildWall = () => {
-    let wall = new THREE.Group();
-    let row = getRow();
-
-    for (let i = 0; i < 5; i++) {
-        row = getRow(i);
-        wall.add(row);
-    }
-
-    scene.add(wall);
-}
-
-const getRow = (yOffset) => {
-    let row = new THREE.Group();
-
-    for (let i = 0; i < 5; i++) {
-        let brick = getBrick();
-        brick.position.x += (12 + 2) * i;
-        row.add(brick);
-    }
-
-    row.position.y += 10 * yOffset;
-    return row;
-}
 
 const getBrick = () => {
     let length = 12, width = 8;
@@ -109,7 +86,121 @@ const getBrick = () => {
     return new THREE.Mesh(geometry, material);
 }
 
+const getRow = (numBricks, yOffset) => {
+    let row = new THREE.Group();
 
+    for (let i = 0; i < numBricks; i++) {
+        let brick = getBrick();
+        brick.position.x += (12 + 2) * i;
+        row.add(brick);
+    }
+
+    row.position.y += 10 * yOffset;
+    return row;
+}
+
+const getWall = (numBricks) => {
+    let wall = new THREE.Group();
+    let row = getRow();
+
+    for (let i = 0; i < 5; i++) {
+        row = getRow(numBricks, i);
+        wall.add(row);
+    }
+
+    wall.position.x -= 50;
+    wall.position.y -= 25;
+    return wall;
+}
+
+const getWalls = () => {
+    let wallOne = getWall(8);
+    wallOne.rotation.y += 30;
+
+    let wallTwo = getWall(8);
+    wallTwo.rotation.y += 30;
+    wallTwo.position.x += 150;
+    wallTwo.position.z -= 30;
+
+    let wallThree = getWall(12);
+    wallThree.rotation.y -= 2.95;
+    wallThree.position.x += 145;
+    wallThree.position.z -= 30;
+
+    let three = new THREE.Group();
+    three.add(wallOne);
+    three.add(wallTwo);
+    three.add(wallThree);
+
+    three.rotation.y -= 0.2;
+    three.position.z -= 100;
+    three.position.x += 20;
+
+    let wallFour = getWall(12);
+    wallFour.position.z = 2;
+
+    let walls = new THREE.Group();
+    walls.add(three);
+    walls.add(wallFour);
+
+    return walls;
+}
+
+const getTower = () => {
+    let tower = new THREE.Group();
+
+    let cylinderGeo = new THREE.CylinderGeometry(20, 20, 70, 64, 64),
+        cylinderMat = new THREE.MeshLambertMaterial({ color: 0xb0b0b0 }),
+        cylinderMesh = new THREE.Mesh(cylinderGeo, cylinderMat);
+
+    let roofGeo = new THREE.ConeGeometry(25, 25, 64, 64),
+        roofMat = new THREE.MeshLambertMaterial({ color: 0xd65b42 }),
+        roofMesh = new THREE.Mesh(roofGeo, roofMat);
+
+    roofMesh.position.y += 45;
+    tower.position.y += 10;
+
+    tower.add(cylinderMesh);
+    tower.add(roofMesh);
+    return tower;
+}
+
+const getTowers = () => {
+    let towers = new THREE.Group();
+
+    let tower = getTower();
+    tower.position.x += 55;
+    tower.position.z += 7;
+    towers.add(tower);
+
+    let towerTwo = getTower();
+    towerTwo.position.x -= 90;
+    towerTwo.position.z += 5;
+    towers.add(towerTwo);
+
+    let towerThree = getTower();
+    towerThree.position.x -= 85;
+    towerThree.position.z -= 115;
+    towers.add(towerThree);
+
+    let towerFour = getTower();
+    towerFour.position.x += 63
+    towerFour.position.z -= 112;
+    towers.add(towerFour);
+
+    return towers;
+}
+
+const buildCastle = () => {
+
+    let walls = getWalls();
+    walls.position.x -= 50;
+    scene.add(walls);
+
+    let towers = getTowers();
+    scene.add(towers);
+
+}
 
 // RANOM HELPER FUCNTIONS
 const randomFloatGenerator = (min, max) => Math.random() * (max - min) + min;
@@ -117,5 +208,5 @@ const randomIntegerGenerator = (min, max) => Math.floor(Math.random() * (max - m
 
 init();
 animate();
-buildWall();
+buildCastle();
 render();
